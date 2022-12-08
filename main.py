7b89bcdc562cd9f3350d2fe392043315197b3815
jangramohit406 <120076515+jangramohit406@users.noreply.github.com>
2022-12-08 09:34:15 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,27 @@
+n = int(input("enter month no. :"))
+if n== 1 :
+    print ("it is january")
+elif n== 2 :
+    print ("its february")
+elif n== 3 :
+    print ("its march")
+elif n== 4:
+    print ("its april")
+elif n== 5 :
+    print ("its may")
+elif n== 6 :
+    print ("its june")
+elif n== 7 :
+    print ("its july")
+elif n== 8 :
+    print ("its august")
+elif n== 9 :
+    print ("its september")
+elif n== 10 :
+    print ("its october")
+elif n== 11:
+    print ("its november")
+elif n== 12 :
+    print ("its december")
+else :
+    print("you enter invalid no.")
